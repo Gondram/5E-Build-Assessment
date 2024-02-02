e60770ca0e01126bdf282b09cc80dd3ad7c38399
Gondram <gondram@gmail.com>
2024-02-01 23:23:33 -0500

    for loop

---
+++ custom_functions.py
@@ -0,0 +1,34 @@
+class Process:
+    def __init__(self):
+        pass
+
+    def spell_attack(self, arg):
+        print(f'arg = {arg}')
+
+    def attack_roll(self, arg):
+        print(f'arg = {arg}')
+
+    def multi_target(self, arg):
+        print(f'arg = {arg}')
+        
+    def dice_damage(self, arg):
+        print(f'arg = {arg}')
+        
+    def damage_type(self, arg):
+        print(f'arg = {arg}')
+        
+    def mt_cantrip_upcast(self, arg):
+        print(f'arg = {arg}')
+        
+    def school(self, arg):
+        print(f'arg = {arg}')
+        
+    def spell(self, arg):
+        print(f'arg = {arg}')
+        
+    def action(self, arg):
+        print(f'arg = {arg}')
+        
+    def dice_cantrip_upcast(self, arg):
+        print(f'arg = {arg}')
+
